664194291c4a30492ef3fbebf7bb24dd28503f24
punishermortal <51474998+punishermortal@users.noreply.github.com>
2022-12-10 02:21:27 +0530

    Time: 251 ms (73.44%), Space: 0B (100.00%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -2,10 +2,4 @@
 #select ifnull distinct(salary) as secondHighestSalary from employee 
 #order by salary desc
 #limit 1,1
-SELECT
-    IFNULL(
-      (SELECT DISTINCT Salary
-       FROM Employee
-       ORDER BY Salary DESC
-        LIMIT 1 OFFSET 1),
-    NULL) AS SecondHighestSalary
+select max(salary) as secondhighestsalary from employee where salary !=(select max(salary) from employee)
