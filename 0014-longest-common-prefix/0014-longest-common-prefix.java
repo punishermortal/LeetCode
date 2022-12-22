0b74d848b4977507952c80b7d24aabc9b20345c4
punishermortal <51474998+punishermortal@users.noreply.github.com>
2022-12-22 14:19:27 +0530

    Time: 9 ms (23.69%), Space: 41.2 MB (76.49%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,16 +1,29 @@
+//["aadog","racecar","car"]====>output ""
+
 class Solution {
     public String longestCommonPrefix(String[] strs) {
+        //length of a strs array is n
         int n=strs.length;
+        //take a pointer as name j
         int j=0;
+        //that is the first element in array strs
         String st=strs[0];
+        //length of of string
         int k=st.length();
+        //variable for storing ans
         String ans="";
+        
+        //lets optimize this ques ,not think of n^2 try to solve it
+        //in length of first string *length of array
         while(j<k){
             for(int i=0;i<n;i++){
-                if(strs[i].length()<=j || st.charAt(j)!=strs[i].charAt(j)){
+                //check if the length of string str[i] is less than j
+                //then return ans and if char is diffrent then also return ans
+                if(strs[i].length()<=j || st.charAt(j) != strs[i].charAt(j)){
                     return ans;
                 }
             }
+            //add the character in ans
             ans+=st.charAt(j);
             j++;
         }
