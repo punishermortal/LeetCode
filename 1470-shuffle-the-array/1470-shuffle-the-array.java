4949e66baf28cb170ad1e70b2b2dba689603df11
punishermortal <51474998+punishermortal@users.noreply.github.com>
2023-04-02 01:43:36 +0530

    Time: 0 ms (100.00%), Space: 43.1 MB (7.42%) - LeetHub

---
+++ 1470-shuffle-the-array/1470-shuffle-the-array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int[]ans=new int[2*n];
+        int j=0;
+        for(int i=0;i<n;i++){
+            ans[j]=nums[i];
+            ans[j+1]=nums[n+i];
+            j=j+2;
+        }
+        return ans;
+    }
+}
