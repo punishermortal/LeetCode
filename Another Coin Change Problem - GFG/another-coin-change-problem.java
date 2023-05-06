fa23b211f5865e242a87faf1fd8167187c2b2f4d
punishermortal <51474998+punishermortal@users.noreply.github.com>
2023-05-06 23:54:34 +0530

    Added solution - LeetHub

---
+++ Another Coin Change Problem - GFG/another-coin-change-problem.java
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class IntArray {
+    public static int[] input(BufferedReader br, int n) throws IOException {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) a[i] = Integer.parseInt(s[i]);
+
+        return a;
+    }
+
+    public static void print(int[] a) {
+        for (int e : a) System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<Integer> a) {
+        for (int e : a) System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+
+            int N;
+            N = Integer.parseInt(br.readLine());
+
+            int K;
+            K = Integer.parseInt(br.readLine());
+
+            int target;
+            target = Integer.parseInt(br.readLine());
+
+            int[] coins = IntArray.input(br, N);
+
+            Solution obj = new Solution();
+            boolean res = obj.makeChanges(N, K, target, coins);
+
+            int _result_val = (res) ? 1 : 0;
+            System.out.println(_result_val);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public static boolean makeChanges(int N, int K, int target, int[] coins) {
+        // code here
+        boolean [][]dp = new boolean[K+1][target+1];
+        dp[0][0]=true;
+        for(int i=0;i<N;i++){
+            for(int j=1;j<=K;j++){
+                for(int l=1;l<=target;l++){
+                    if(coins[i]<=l && dp[j][l]==false)
+                    dp[j][l] = dp[j-1][l-coins[i]];
+                }
+            }
+        }
+        return dp[K][target];
+    }
+}
