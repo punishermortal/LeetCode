a8886bc26468adcbb717cfd95872eba57a6f9415
punishermortal <51474998+punishermortal@users.noreply.github.com>
2023-05-10 23:23:33 +0530

    Added solution - LeetHub

---
+++ Total Cuts - GFG/total-cuts.java
@@ -0,0 +1,85 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+class IntArray
+{
+    public static int[] input(BufferedReader br, int n) throws IOException
+    {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++)
+            a[i] = Integer.parseInt(s[i]);
+
+        return a;
+    }
+
+    public static void print(int[] a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<Integer> a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            int N;
+            N = Integer.parseInt(br.readLine());
+            
+            
+            int K;
+            K = Integer.parseInt(br.readLine());
+            
+            
+            int[] A = IntArray.input(br, N);
+            
+            Solution obj = new Solution();
+            int res = obj.totalCuts(N, K, A);
+            
+            System.out.println(res);
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public static int totalCuts(int n, int k, int[] arr) {
+        // code here
+        int[]left =new int[n];
+        left[0]=arr[0];
+        int[]right=new int[n];
+        right[0]=arr[n-1];
+        for(int i=1;i<n;i++){
+            left[i] = Math.max(left[i-1],arr[i]);
+        }
+        right[n-1]=arr[n-1];
+        for(int i=n-2;i>=0;i--){
+            right[i] = Math.min(right[i+1],arr[i]);
+        }
+        int ans=0;
+        for(int i=1;i<n;i++){
+            if(left[i-1]+right[i]>=k){
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
+        
