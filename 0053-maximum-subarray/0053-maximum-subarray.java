51f861f250f71c69b7b5b890f53749bd024e2dc0
punishermortal <51474998+punishermortal@users.noreply.github.com>
2023-05-23 22:28:43 +0530

    Time: 1 ms (100.00%), Space: 58.6 MB (5.07%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,15 +1,20 @@
 class Solution {
     public int maxSubArray(int[] nums) {
+        //variable sum which store the sum  of value of array
         int sum = nums[0];
-        int cursum = nums[0];
+        //beatsum
+        int beat_sum = nums[0];
         for(int i=1;i<nums.length;i++){
+            //when sum will be positive only then it will be added otherwise new current 
+            //sum will be start from that index
             if(sum>0){
                 sum+=nums[i];
             }else{
                 sum=nums[i];
             }
-            cursum=Math.max(cursum,sum);
+            //update the ans only if it beat the current sum
+            beat_sum=Math.max(beat_sum,sum);
         }
-        return cursum;
+        return beat_sum;
     }
 }
