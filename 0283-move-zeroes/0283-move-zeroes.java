d402d3f1a3813fba271631e24e14961f34c55750
punishermortal <51474998+punishermortal@users.noreply.github.com>
2023-09-06 00:16:45 +0530

    Time: 2 ms (58.82%), Space: 45.3 MB (31.75%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,15 +1,27 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int val=0;
-        for(int i:nums){
-            if(i!=0){
-                nums[val]=i;
-                val++;
+        // int val=0;
+        // for(int i:nums){
+        //     if(i!=0){
+        //         nums[val]=i;
+        //         val++;
+        //     }
+        // }
+        // while(val<=nums.length - 1){
+        //     nums[val]=0;
+        //     val++;
+        // }
+        int nonZeroIndex=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=0){
+                if(nonZeroIndex != i){
+                    //swap
+                    int temp=nums[nonZeroIndex];
+                    nums[nonZeroIndex]=nums[i];
+                    nums[i]=temp;
+                }
+                nonZeroIndex++;
             }
-        }
-        while(val<=nums.length - 1){
-            nums[val]=0;
-            val++;
         }
     }
 }
